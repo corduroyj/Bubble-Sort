41943bf6c2a6bd41bae77534ff287364d61c7943
Corduroy J <gabejonuk@gmail.com>
2021-08-14 15:20:47 -0700

    Update bubbly-sort.rb
    Deleted debug puts and comments

--- bubbly-sort.rb
+++ bubbly-sort.rb
@@ -1,13 +1,6 @@
-#Get mishmatched Array
-#move down array and check 2 numbers
-#if first one is larger than second then swap
-#move to next cell
-#after you move fully through array set last one and restart
-
 def bubble_sort (array)
   #define default values 
   len = array.length() 
-  puts "len is #{len}"
   i = 0
   while i <= len do 
     if len == 0 
@@ -15,15 +8,10 @@ def bubble_sort (array)
     elsif i == len - 1
       len = len - 1
       i = -1
-      puts "Found end of array"
-      puts "len is #{len}"
     elsif array[i] > array[i + 1]
       array = swap(array, i)
-      puts "Found Swap"
     end
     i += 1
-    puts "i is #{i}"
-    p array
   end
   return array
 end
